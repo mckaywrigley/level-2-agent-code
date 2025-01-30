491b2c1e7b1b07ca3135f8080ab76c7b8730a0ca
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-30 16:00:04 -0700

    more messaging changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function HomePage() {
     <div className="flex-1 p-4 pt-0">
       <h1>Welcome to the Level 2 Coding Agent Lesson</h1>
 
-      <Link href="/about">About Page</Link>
+      <Link href="/about">Go to the About Page</Link>
     </div>
   )
 }
